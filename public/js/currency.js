2fa84d0d0f355d71015af66b7928edcd4befaf94
Tóth Ádám <tothadam1010@gmail.com>
2024-04-14 15:02:19 +0200

    Fully created the currency website

--- public/js/currency.js
+++ public/js/currency.js
@@ -2,12 +2,19 @@ const inputCheckBox = document.getElementById("checkbox")
 const queryButton = document.getElementById("query")
 const divInputResult = document.querySelector(".input-result")
 const inputExchange = document.getElementById("exchange-code")
+const clearBtn = document.getElementById("clear")
 let index = 34
 
 inputCheckBox.addEventListener("change", () => {
     const checkboxClickDiv = document.getElementById("checkboxClickDiv")
     checkboxClickDiv.classList.toggle("checkbox-click")
     checkboxClickDiv.classList.toggle("checkbox-click-on")
+})
+
+
+clearBtn.addEventListener("click", () => {
+    const result = document.querySelector(".result")
+    result.remove()
 })
 
 
@@ -22,91 +29,140 @@ queryButton.addEventListener("click", () => {
     };
 
     if (inputCheckBox.checked) {
-        
-    } else {
-        fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols=&base=${inputExchange.value}`, requestOptions)
+        const checkboxClickDiv = document.getElementById("checkboxClickDiv")
+        fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${checkboxClickDiv.children[0].value}&from=${inputExchange.value}&amount=${checkboxClickDiv.children[1].value}`, requestOptions)
         .then(response => response.json())
         .then(result => {
-            const divResult = document.createElement("div")
-            divResult.classList.add("result")
+            if (result.error) {
+                alert("Hiba! Ellenőrizd az adatokat, amiket beütöttél!")
+            } else {
+                clearBtn.style.display = "block"
 
-            const pResult = document.createElement("p")
-            pResult.innerHTML = "Az értékek"
-            pResult.style.textAlign = "center"
-            pResult.style.fontSize = "2rem"
+                const divResult = document.createElement("div")
+                divResult.classList.add("result")
 
-            const divResultText = document.createElement("div")
-            divResultText.classList.add("result-text")
+                const pResult = document.createElement("p")
+                pResult.innerHTML = "Az értékek"
+                pResult.style.textAlign = "center"
+                pResult.style.fontSize = "2rem"
 
-            const divResultTextDiv = document.createElement("div")
-            divResultTextDiv.classList.add("result-text-div")
+                const divResultText = document.createElement("div")
+                divResultText.classList.add("result-text")
+
+                const divResultTextDiv = document.createElement("div")
+                divResultTextDiv.classList.add("result-text-div")
+
+                
+                divResultText.appendChild(divResultTextDiv)
+                divResult.appendChild(pResult)
+                divResult.appendChild(divResultText)
+                divInputResult.appendChild(divResult)
 
 
-            divResultText.appendChild(divResultTextDiv)
-
-
-            divResult.appendChild(pResult)
-            divResult.appendChild(divResultText)
-            divInputResult.appendChild(divResult)
-
-            let keys = Object.keys(result.rates)
-            let values = Object.values(result.rates)
-
-            for (let i = 0; i < index; i++) {
                 const divResultTextSpan = document.createElement("div")
                 divResultTextSpan.classList.add("result-text-span")
 
                 const spanKey = document.createElement("span")
                 const spanValue = document.createElement("span")
 
-                spanKey.innerHTML = keys[i]
-                spanValue.innerHTML = values[i]
+                spanKey.innerHTML = result.query.to
+                spanValue.innerHTML = Number(result.result).toFixed(4)
 
-                delete keys[i]
-                delete values[i]
 
                 divResultTextSpan.appendChild(spanKey)
                 divResultTextSpan.appendChild(spanValue)
 
                 divResultTextDiv.appendChild(divResultTextSpan)
             }
+        })
+    } else {
+        fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols=&base=${inputExchange.value}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            if (result.error) {
+                alert("Hiba! Ellenőrizd az adatokat, amiket beütöttél!")
+            } else {
+                clearBtn.style.display = "block"
 
-            
-            const loadMoreButton = document.createElement("button")
-            loadMoreButton.innerHTML = "További betöltése"
-
-            divResult.appendChild(loadMoreButton)
-
-
-            loadMoreButton.addEventListener("click", () => {
-                if (keys[169] != undefined) {
-                    const divResultTextDiv = document.createElement("div")
-                    divResultTextDiv.classList.add("result-text-div")
-                    divResultText.appendChild(divResultTextDiv)
-                    for (let i = index; i < index + 34; i++) {
-                        const divResultTextSpan = document.createElement("div")
-                        divResultTextSpan.classList.add("result-text-span")
-        
-                        const spanKey = document.createElement("span")
-                        const spanValue = document.createElement("span")
-        
-                        spanKey.innerHTML = keys[i]
-                        spanValue.innerHTML = values[i]
-        
-                        delete keys[i]
-                        delete values[i]
-        
-                        divResultTextSpan.appendChild(spanKey)
-                        divResultTextSpan.appendChild(spanValue)
-        
-                        divResultTextDiv.appendChild(divResultTextSpan)
-                    }
-                } else {
-                    return
+                const divResult = document.createElement("div")
+                divResult.classList.add("result")
+    
+                const pResult = document.createElement("p")
+                pResult.innerHTML = "Az értékek"
+                pResult.style.textAlign = "center"
+                pResult.style.fontSize = "2rem"
+    
+                const divResultText = document.createElement("div")
+                divResultText.classList.add("result-text")
+    
+                const divResultTextDiv = document.createElement("div")
+                divResultTextDiv.classList.add("result-text-div")
+    
+    
+                divResultText.appendChild(divResultTextDiv)
+                divResult.appendChild(pResult)
+                divResult.appendChild(divResultText)
+                divInputResult.appendChild(divResult)
+    
+                let keys = Object.keys(result.rates)
+                let values = Object.values(result.rates)
+    
+                for (let i = 0; i < index; i++) {
+                    const divResultTextSpan = document.createElement("div")
+                    divResultTextSpan.classList.add("result-text-span")
+    
+                    const spanKey = document.createElement("span")
+                    const spanValue = document.createElement("span")
+    
+                    spanKey.innerHTML = keys[i]
+                    spanValue.innerHTML = Number(values[i]).toFixed(4)
+    
+                    delete keys[i]
+                    delete values[i]
+    
+                    divResultTextSpan.appendChild(spanKey)
+                    divResultTextSpan.appendChild(spanValue)
+    
+                    divResultTextDiv.appendChild(divResultTextSpan)
                 }
-
-                index += 34
-            })
+    
+                
+                const loadMoreButton = document.createElement("button")
+                loadMoreButton.innerHTML = "További betöltése"
+    
+                divResult.appendChild(loadMoreButton)
+    
+    
+                loadMoreButton.addEventListener("click", () => {
+                    if (keys[169] != undefined) {
+                        const divResultTextDiv = document.createElement("div")
+                        divResultTextDiv.classList.add("result-text-div")
+                        divResultText.appendChild(divResultTextDiv)
+                        for (let i = index; i < index + 34; i++) {
+                            const divResultTextSpan = document.createElement("div")
+                            divResultTextSpan.classList.add("result-text-span")
+            
+                            const spanKey = document.createElement("span")
+                            const spanValue = document.createElement("span")
+            
+                            spanKey.innerHTML = keys[i]
+                            spanValue.innerHTML = values[i]
+            
+                            delete keys[i]
+                            delete values[i]
+            
+                            divResultTextSpan.appendChild(spanKey)
+                            divResultTextSpan.appendChild(spanValue)
+            
+                            divResultTextDiv.appendChild(divResultTextSpan)
+                        }
+                    } else {
+                        return
+                    }
+    
+                    index += 34
+                })
+            }
         })
     }
 })
