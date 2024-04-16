01affeb51e5c83ed529a6d48d486b7a21bb8f6ec
Adam Toth <tothadam1010@gmail.com>
2024-04-16 12:08:31 +0200

    Madee the request fully working for stock website

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 const express = require("express")
-const request = require("request")
 const router = express.Router()
+const request = require("request")
 
 router.get("/", (req, res) => {
     res.render("index")
@@ -16,6 +16,25 @@ router.get("/stock-market", (req, res) => {
 
 router.get("/currency", (req, res) => {
     res.render("currency")
+})
+
+router.post("/stock-prices", (req, res) => {
+    let TDKEY = "7852a1e3df16458b858733c4764e161a"
+    var tdurl = `https://api.twelvedata.com/price?symbol=${req.body.input.toString()}&apikey=${TDKEY}`
+
+    request.get({
+        url: tdurl,
+        json: true,
+        headers: { 'User-Agent': 'request' }
+    }, (err, response, data) => {
+        if (err) {
+            console.log('Error:', err);
+        } else if (response.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+        } else {
+            res.status(200).json({ message: data})
+        }
+    })
 })
 
 
--- server.js
+++ server.js
@@ -16,30 +16,4 @@ app.use(express.static(path.join(initialPath)));
 app.use("/", router)
 
 
-router.post("/stock-prices", (req, res) => {
-    console.log(req.body);
-    res.status(200).json({ message: req.body })
-    // let TDKEY = "7852a1e3df16458b858733c4764e161a"
-
-    // async function CurrentPrices(tickers) {
-    //     var tdurl = `https://api.twelvedata.com/price?symbol=${tickers.toString()}&apikey=${TDKEY}`;
-
-    //     request.get({
-    //         url: tdurl,
-    //         json: true,
-    //         headers: { 'User-Agent': 'request' }
-    //     }, (err, res, data) => {
-    //         if (err) {
-    //             console.log('Error:', err);
-    //         } else if (res.statusCode !== 200) {
-    //             console.log('Status:', res.statusCode);
-    //         } else {
-    //             console.log(data);
-    //         }
-    //     });
-    // }
-    // CurrentPrices(["IBM", "TSLA"])
-})
-
-
 app.listen(process.env.PORT || 1000)
